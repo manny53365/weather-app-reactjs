f8f374ef2822c21af7d845e863c802ee8a2288d6
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-02-06 00:53:25 +0530

    added some styling to the landing page

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -26,49 +26,52 @@ export default function Home() {
     };
 
   return (
-    <form className={styles['user-input-form']} onSubmit={handleSubmit}>
+    <div>
         <h1>Weather App</h1>
-        {!data && (
-        <>
-            <label>
-                <input type="text" placeholder='Enter City Name*' required onChange={event => setCity(event.target.value)} value={city}/>
-            </label>
-            <label>
-                <input type="text" placeholder='Enter State Code:' onChange={event => setState(event.target.value)} value={state} />
-            </label>
-            <label>
-                <input type="text" placeholder='Enter Country Code:' onChange={event => setCountryCode(event.target.value)} value={countryCode} />
-            </label>
-            <label>
-                <select id="units" required onChange={event => setUnits(event.target.value)} value={units}>
-                    <option value="">---MAKE A SELECTION---</option>
-                    <option value="metric">Celsius</option>
-                    <option value="imperial">Fahrenheit</option>
-                </select>
-            </label>
-            <button type='submit'>Submit</button>
-        </>
-        )}
-        {data && (
-            <div>
-                <h2>{data.name}</h2>
-                <div className={styles['temperatures']}>
-                    <p>Current Temp: {Math.round(data.main.feels_like)}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F': 'K'}</p>
-                    <p>Feels Like: {Math.round(data.main.temp)}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F': 'K'}</p>
-                    <p>High: {Math.round(data.main.temp_max)}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F': 'K'}</p>
-                    <p>Humidity: {Math.round(data.main.humidity)}</p>
+        <form className={styles['user-input-form']} onSubmit={handleSubmit}>
+            {!data && (
+            <>
+                <label>
+                    <input type="text" placeholder='Enter City Name*' required onChange={event => setCity(event.target.value)} value={city}/>
+                </label>
+                <label>
+                    <input type="text" placeholder='Enter State Code:' onChange={event => setState(event.target.value)} value={state} />
+                </label>
+                <label>
+                    <input type="text" placeholder='Enter Country Code:' onChange={event => setCountryCode(event.target.value)} value={countryCode} />
+                </label>
+                <label>
+                    Select a unit of measurement:
+                    <select id="units" required onChange={event => setUnits(event.target.value)} value={units}>
+                        <option value="">---MAKE A SELECTION---</option>
+                        <option value="metric">Celsius</option>
+                        <option value="imperial">Fahrenheit</option>
+                    </select>
+                </label>
+                <button type='submit'>Submit</button>
+            </>
+            )}
+            {data && (
+                <div>
+                    <h2>{data.name}</h2>
+                    <div className={styles['temperatures']}>
+                        <p>Current Temp: {Math.round(data.main.feels_like)}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F': 'K'}</p>
+                        <p>Feels Like: {Math.round(data.main.temp)}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F': 'K'}</p>
+                        <p>High: {Math.round(data.main.temp_max)}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F': 'K'}</p>
+                        <p>Humidity: {Math.round(data.main.humidity)}</p>
+                    </div>
+                    <div className={'wind-speeds'}>
+                        <p>Wind Speed: {data.wind.speed}</p>
+                    </div>
+                    <div style={styles['sun-info']}>
+                        <p>Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}</p>
+                        <p>Sunset: {new Date(data.sys.sunset * 1000).toLocaleTimeString()}</p>
+                    </div>
+                    <button type='submit' onClick={handleReset}>Search again</button>
                 </div>
-                <div className={'wind-speeds'}>
-                    <p>Wind Speed: {data.wind.speed}</p>
-                </div>
-                <div style={styles['sun-info']}>
-                    <p>Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}</p>
-                    <p>Sunset: {new Date(data.sys.sunset * 1000).toLocaleTimeString()}</p>
-                </div>
-                <button type='submit' onClick={handleReset}>Search again</button>
-            </div>
-        )}
-        {error && <p>{error}</p>}
-    </form>
+            )}
+            {error && <p>{error}</p>}
+        </form>
+    </div>
   )
 }
