634701f751ce5e73a70e66d175b905dea3ca31a9
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-02-06 00:44:26 +0530

    added custom hook to handle calls to the open weather api and provide the data on screen to the user

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useOpenWeather } from '../hooks/useOpenWeather';
 
 import styles from './Home.module.css';
 
@@ -8,36 +9,66 @@ export default function Home() {
     const [state, setState] = useState('');
     const [countryCode, setCountryCode] = useState('');
     const [units, setUnits] = useState('');
+    const { searchWeatherByCityName, error, data, reset } = useOpenWeather();
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
-        console.log('I was submitted');
-        console.log(city, state, countryCode, units);
+        await searchWeatherByCityName(city,units);
+    };
+
+    const handleReset = (event) => {
+        event.preventDefault();
+        setCity('');
+        setState('');
+        setCountryCode('');
+        setUnits('');
+        reset();
     };
 
   return (
     <form className={styles['user-input-form']} onSubmit={handleSubmit}>
-        <h2>Weather App</h2>
-        <label>
-            City Name:
-            <input type="text" placeholder='Enter City Name*' required onChange={event => setCity(event.target.value)} value={city}/>
-        </label>
-        <label>
-            State Code:
-            <input type="text" placeholder='Enter State Code:' onChange={event => setState(event.target.value)} value={state} />
-        </label>
-        <label>
-            Country Code:
-            <input type="text" placeholder='Enter Country Code:' onChange={event => setCountryCode(event.target.value)} value={countryCode} />
-        </label>
-        <label>
-            <select id="units" required onChange={event => setUnits(event.target.value)} value={units}>
-                <option value="">---MAKE A SELECTION---</option>
-                <option value="metric">Celsius</option>
-                <option value="imperial">Fahrenheit</option>
-            </select>
-        </label>
-        <button type='submit'>Submit</button>
+        <h1>Weather App</h1>
+        {!data && (
+        <>
+            <label>
+                <input type="text" placeholder='Enter City Name*' required onChange={event => setCity(event.target.value)} value={city}/>
+            </label>
+            <label>
+                <input type="text" placeholder='Enter State Code:' onChange={event => setState(event.target.value)} value={state} />
+            </label>
+            <label>
+                <input type="text" placeholder='Enter Country Code:' onChange={event => setCountryCode(event.target.value)} value={countryCode} />
+            </label>
+            <label>
+                <select id="units" required onChange={event => setUnits(event.target.value)} value={units}>
+                    <option value="">---MAKE A SELECTION---</option>
+                    <option value="metric">Celsius</option>
+                    <option value="imperial">Fahrenheit</option>
+                </select>
+            </label>
+            <button type='submit'>Submit</button>
+        </>
+        )}
+        {data && (
+            <div>
+                <h2>{data.name}</h2>
+                <div className={styles['temperatures']}>
+                    <p>Current Temp: {Math.round(data.main.feels_like)}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F': 'K'}</p>
+                    <p>Feels Like: {Math.round(data.main.temp)}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F': 'K'}</p>
+                    <p>High: {Math.round(data.main.temp_max)}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F': 'K'}</p>
+                    <p>Humidity: {Math.round(data.main.humidity)}</p>
+                </div>
+                <div className={'wind-speeds'}>
+                    <p>Wind Speed: {data.wind.speed}</p>
+                </div>
+                <div style={styles['sun-info']}>
+                    <p>Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}</p>
+                    <p>Sunset: {new Date(data.sys.sunset * 1000).toLocaleTimeString()}</p>
+                </div>
+                <button type='submit' onClick={handleReset}>Search again</button>
+            </div>
+        )}
+        {error && <p>{error}</p>}
     </form>
   )
 }
--- src/hooks/useOpenWeather.jsx
+++ src/hooks/useOpenWeather.jsx
@@ -1,21 +1,23 @@
-import 'dotenv/config';
 import { useState } from 'react';
 import axios from 'axios';
 
 export const useOpenWeather = () => {
 
   const [error, setError] = useState(null);
-  const APIKey = process.env.APP_API_KEY;
+  const [data, setData] = useState('');
+  const APIKey = '086194c2daa42405db72f49319d70778';
 
   const searchWeatherByCityName = async (city, units) => {
     setError(null);
 
     try {
       if (units !== ''){
-        await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${units}&appid=${APIKey}`);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${APIKey}`);
+        setData(response.data);
         setError(null);
       } else {
-        await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`);
+        setData(response.data);
         setError(null);
       }
     } catch (err) {
@@ -23,5 +25,9 @@ export const useOpenWeather = () => {
     };
   };
 
-  return {searchWeatherByCityName, error}
+  const reset = () => {
+    setData('');
+  };
+
+  return {searchWeatherByCityName, reset, error, data}
 }
