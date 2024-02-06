547bcde60c96816c09ce38ef15d1dff074883d72
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-02-06 15:02:19 +0530

    added some starter code to the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from './home/Home';
 
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Footer />
     </div>
   );
 }
